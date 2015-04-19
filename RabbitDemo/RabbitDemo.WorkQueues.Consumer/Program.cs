a36ca9c291bc10cff6c59c7dd09a09afbba6740d
Anders Ljusberg <andlju@gmail.com>
2015-04-19 21:10:29 +0200

    More comments

--- RabbitDemo/RabbitDemo.WorkQueues.Consumer/Program.cs
+++ RabbitDemo/RabbitDemo.WorkQueues.Consumer/Program.cs
@@ -17,15 +17,19 @@ namespace RabbitDemo.WorkQueues.Consumer
             using (var connection = connectionFactory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
+                // Make sure the simple-work-queue exists
                 channel.QueueDeclare("simple-work-queue", true, false, false, null);
-                channel.BasicQos(0, 1, false);
 
+                // Set prefetch to 1 (only fetch one message at a time)
+                // channel.BasicQos(0, 1, false);
+
+                // Set up internal consumer queue. This will try to get as many messages as possible
                 var consumer = new QueueingBasicConsumer(channel);
-
                 channel.BasicConsume("simple-work-queue", false, consumer);
 
                 while (true)
                 {
+                    // Fetch a new message from the internal queue
                     var message = consumer.Queue.Dequeue();
                     var contents = message.Body.GetString();
                     
